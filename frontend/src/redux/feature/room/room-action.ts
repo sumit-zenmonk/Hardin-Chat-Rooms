c35e37b349a38808d9c38a507fc25a4e95037f37
Sumit Kumar <sumit.1183@zenmonk.tech>
2026-06-16 13:34:17 +0530

    correct name used

--- frontend/src/redux/feature/room/room-action.ts
+++ frontend/src/redux/feature/room/room-action.ts
@@ -11,7 +11,7 @@ export const createRoom = createAsyncThunk<
     CreateRoomPayload,
     { state: RootState }
 >(
-    "wallet/add-amount",
+    "room/post",
     async (payload, { getState, rejectWithValue }) => {
         try {
             const token = getState().authReducer.token || "";
