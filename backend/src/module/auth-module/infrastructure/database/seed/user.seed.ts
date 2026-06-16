20fd42db3acc54effa04d35d6b8e841e34c257fc
Sumit Kumar <sumit.1183@zenmonk.tech>
2026-06-16 18:06:51 +0530

    id removed from seeding

--- backend/src/module/auth-module/infrastructure/database/seed/user.seed.ts
+++ backend/src/module/auth-module/infrastructure/database/seed/user.seed.ts
@@ -6,14 +6,12 @@ import { UserEntity } from '../../../domain/user/user.entity';
 // hardcoded users for all microservices
 const users = [
     {
-        id: 1,
         uuid: 'c0a80101-7b1d-4a9f-8c1a-123456789001',
         email: 'user1@gmail.com',
         name: 'user 1',
         created_at: new Date('2025-01-01T00:00:00.000Z'),
     },
     {
-        id: 2,
         uuid: 'c0a80102-7b1d-4a9f-8c1a-123456789002',
         email: 'user2@gmail.com',
         name: 'user 2',
@@ -41,7 +39,6 @@ async function create() {
         // use same users across all services
         for (const user of users) {
             const created_user = await queryRunner.manager.save(UserEntity, {
-                id: user.id,
                 uuid: user.uuid,
                 email: user.email,// faker.internet.email(),
                 password: hashedPassword,
