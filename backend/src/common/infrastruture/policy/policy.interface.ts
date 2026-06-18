5b969d9c55171f2275276c4df3d455b77ce55f79
Sumit Kumar <sumit.1183@zenmonk.tech>
2026-06-18 18:17:54 +0530

    remove unused code

--- backend/src/common/infrastruture/policy/policy.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface PolicyInterfaceService {
-    handleSetPolicy(key: string, policy: any): Promise<void>;
-
-    handleGetPolicy(key: string): Promise<any>;
-
-    handleRemovePolicy(key: string): void;
-
-    handleProcess(key: string): Promise<any>;
-}
